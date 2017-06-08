d8a096def3be58f71dedbd1b84a086501d9f0929
chensheng <jrchens@126.com>
2017-06-09 00:15:24 +0800

    remove Base.java (disabled,deleted,cruser,crtime,mduser,mdtime)

--- tsl-common/src/main/java/me/simple/domain/SysGroup.java
+++ tsl-common/src/main/java/me/simple/domain/SysGroup.java
@@ -1,27 +1,106 @@
 package me.simple.domain;
 
-public class SysGroup extends Base {
+import me.simple.validator.group.Remove;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
-private int id;
-private String groupname;
-private String viewname;
+import java.sql.Timestamp;
 
-public int getId(){
-  return id;
-}
-public void setId(int id){
-  this.id = id;
-}
-public String getGroupname(){
-  return groupname;
-}
-public void setGroupname(String groupname){
-  this.groupname = groupname;
-}
-public String getViewname(){
-  return viewname;
-}
-public void setViewname(String viewname){
-  this.viewname = viewname;
-}
+public class SysGroup implements java.io.Serializable {
+
+    private int id;
+    private String groupname;
+    private String viewname;
+
+    private boolean disabled;
+    private boolean deleted;
+    private String cruser;
+    private Timestamp crtime;
+    private String mduser;
+    private Timestamp mdtime;
+
+    // ============================================
+    @Length(min = 1,max = 200,groups = {Remove.class})
+    private String ids;
+
+    // ============================================
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+
+    public String getViewname() {
+        return viewname;
+    }
+
+    public void setViewname(String viewname) {
+        this.viewname = viewname;
+    }
+
+    public String getIds() {
+        return ids;
+    }
+
+    public void setIds(String ids) {
+        this.ids = ids;
+    }
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getCruser() {
+        return cruser;
+    }
+
+    public void setCruser(String cruser) {
+        this.cruser = cruser;
+    }
+
+    public Timestamp getCrtime() {
+        return crtime;
+    }
+
+    public void setCrtime(Timestamp crtime) {
+        this.crtime = crtime;
+    }
+
+    public String getMduser() {
+        return mduser;
+    }
+
+    public void setMduser(String mduser) {
+        this.mduser = mduser;
+    }
+
+    public Timestamp getMdtime() {
+        return mdtime;
+    }
+
+    public void setMdtime(Timestamp mdtime) {
+        this.mdtime = mdtime;
+    }
 }
--- tsl-common/src/main/java/me/simple/domain/SysMenu.java
+++ tsl-common/src/main/java/me/simple/domain/SysMenu.java
@@ -3,13 +3,21 @@ package me.simple.domain;
 import me.simple.validator.group.Get;
 import org.hibernate.validator.constraints.Range;
 
-public class SysMenu extends Base {
+import java.sql.Timestamp;
+
+public class SysMenu implements java.io.Serializable {
 
   @Range(min = 1,max = Integer.MAX_VALUE,groups = {Get.class})
   private int id;
 private int pid;
 private String viewname;
 private String url;
+  private boolean disabled;
+  private boolean deleted;
+  private String cruser;
+  private Timestamp crtime;
+  private String mduser;
+  private Timestamp mdtime;
 
 public int getId(){
   return id;
@@ -35,4 +43,52 @@ public String getUrl(){
 public void setUrl(String url){
   this.url = url;
 }
+
+  public boolean isDisabled() {
+    return disabled;
+  }
+
+  public void setDisabled(boolean disabled) {
+    this.disabled = disabled;
+  }
+
+  public boolean isDeleted() {
+    return deleted;
+  }
+
+  public void setDeleted(boolean deleted) {
+    this.deleted = deleted;
+  }
+
+  public String getCruser() {
+    return cruser;
+  }
+
+  public void setCruser(String cruser) {
+    this.cruser = cruser;
+  }
+
+  public Timestamp getCrtime() {
+    return crtime;
+  }
+
+  public void setCrtime(Timestamp crtime) {
+    this.crtime = crtime;
+  }
+
+  public String getMduser() {
+    return mduser;
+  }
+
+  public void setMduser(String mduser) {
+    this.mduser = mduser;
+  }
+
+  public Timestamp getMdtime() {
+    return mdtime;
+  }
+
+  public void setMdtime(Timestamp mdtime) {
+    this.mdtime = mdtime;
+  }
 }
